ed375414daa32b692f4ab26685682216233c8522
João Paulo <musicjoaopaulo@gmail.com>
2018-02-18 19:56:54 -0300

    Add filter pacient function

---
+++ js/filter.js
@@ -0,0 +1,37 @@
+// filter.js
+// filter the name of pacients
+
+//create var for input filter
+var filterInput = document.querySelector("#filter-table");
+console.log(filterInput);
+
+
+//add event when user is typing on input
+filterInput.addEventListener("input", function() {
+  // console.log(this.value);
+
+  //create var for all pacients
+  var pacients = document.querySelectorAll(".paciente");
+  // console.log(pacients);
+
+  if(this.value.length > 0) {
+
+    for (var i = 0; i < pacients.length; i++) {
+      var pacient = pacients[i];
+      var tdName = pacient.querySelector(".info-nome");
+      var name = tdName.textContent;
+      var express = new RegExp(this.value, "i"); //create regular expression
+      if(!express.test(name)) { //validate regular expression
+        pacient.classList.add("invisible");
+      } else {
+        pacient.classList.remove("invisible");
+      }
+    }
+  } else {
+    for (var i = 0; i < pacients.length; i++) {
+      var pacient = pacients[i];
+      pacient.classList.remove("invisible");
+    }
+  }
+
+});
